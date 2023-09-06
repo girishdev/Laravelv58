5b6cee268d642a0493a5556c226c2c54ec4895e6
girishdev <girishkumar.aorg@gmail.com>
2023-09-06 19:38:21 +0530

    Testing-2...

--- project-info/planningNotes.php
+++ project-info/planningNotes.php
@@ -1,20 +1,21 @@
 Testing-1...
+Testing-2...
 
 weeklySummary:
-	1. First Week Summary:
-	2. Second Week Summary:
-	3. Third week Summary:
-	4. FifthWeekPlan.php (4th and 5th Week)
-	5. TodayAndTomorrowTask.php
-	6. SixthWeekPlan.php
-	7. SeventhWeekPlan.php
-	8. EightWeekPlan.php
-	9. NinthWeekPlan.php
+1. First Week Summary:
+2. Second Week Summary:
+3. Third week Summary:
+4. FifthWeekPlan.php (4th and 5th Week)
+5. TodayAndTomorrowTask.php
+6. SixthWeekPlan.php
+7. SeventhWeekPlan.php
+8. EightWeekPlan.php
+9. NinthWeekPlan.php
 
-	Latest: TodayAndTomorrowTask.php
+Latest: TodayAndTomorrowTask.php
 
 Interview-InShort: "This will help in building Resume"
-	Interview Question and Answers InShort
+Interview Question and Answers InShort
 
 LaravelCertificationPlanning:
 
@@ -24,11 +25,11 @@ LaravelCertification: [Xlsx]
 DailyActivities:
 
 InterviewPreparation-Priority:
-	GDrive Index For Priority Topics
-	[Laravel, PHP, MYSQL]
+GDrive Index For Priority Topics
+[Laravel, PHP, MYSQL]
 
 CertificationYouTubeChannel:
-	All About the Laravel Videos
+All About the Laravel Videos
 
 ------------------------------------
 
